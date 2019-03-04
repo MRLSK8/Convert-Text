e6d0c16a076b015c392f245f8979158ad4535746
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-03-03 23:54:09 -0300

    Fist commit: Text_Area added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <textarea className="textarea" placeholder="Texto qualquer">
+          
+        </textarea>
+      </div>
+    );
+  }
+}
+
+export default App;
